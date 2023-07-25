ae65dd45b680b6fdd4b9d87aa4aaeeb80284ac54
Exo <paink37@hotmail.com>
2023-07-25 17:12:26 +0100

    Finished register page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-info border-bottom">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <div class="container py-2">
             <router-link :to="{ name: 'home' }" class="navbar-brand">
                 <span>Another</span>
