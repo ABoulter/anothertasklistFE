31772b9091434be878ce9fc3595af704287db568
Exo <paink37@hotmail.com>
2023-07-25 17:33:06 +0100

    Added Summary to show completed task count

--- src/pages/SummaryPage.vue
+++ src/pages/SummaryPage.vue
@@ -1,3 +1,25 @@
 <template>
-    <h1>Summary</h1>
+    <main class="px-4 py-5 my-5 text-center" style="min-height: 50vh;">
+        <h1 class="display-5 fw-bold mt-4" v-if="showToggleMessage">You're working hard!</h1>
+        <h1 class="display-5 fw-bold mt-4" v-else>Find something cool to do!</h1>
+        <div class="col-lg-6 mx-auto">
+            <p class="lead mb-4" v-if="showToggleMessage">You've completed <strong>{{ completedTasksNumber }}</strong> tasks
+                so far</p>
+        </div>
+    </main>
 </template>
+
+<script setup>
+import { computed } from "vue";
+import { storeToRefs } from "pinia"
+import { useTaskStore } from "../stores/task"
+
+const store = useTaskStore()
+const { completedTasks, uncompletedTasks } = storeToRefs(store)
+
+const completedTasksNumber = completedTasks.value.length
+
+
+const showToggleMessage = computed(() => completedTasks.value.length > 0)
+
+</script>
