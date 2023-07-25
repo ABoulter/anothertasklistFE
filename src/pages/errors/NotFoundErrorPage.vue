baf2be829674b606b9265afbd4c9faa37cc89c54
Exo <paink37@hotmail.com>
2023-07-25 11:35:03 +0100

    Added 404 error handling

---
+++ src/pages/errors/NotFoundErrorPage.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container py-5">
+        <h1>404</h1>
+        <p>Sorry, the page <code>{{ $route.params.notFound }}</code> you're looking for doesn't exist...</p>
+        <router-link :to="{ name: 'tasks' }">Back to your Tasks</router-link>
+    </div>
+</template>
